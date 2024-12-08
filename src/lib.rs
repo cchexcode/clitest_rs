b540083fec4033abb4a25f422fa010b8c17dbaa2
Alexander Weber <aw@voidpointergroup.com>
2024-12-07 16:52:39 -0800

    + cargo_flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use anyhow::Result;
 
 pub struct CliTestSetup {
+    cargo_flags: HashSet<String>,
     program: Vec<String>,
     env: HashMap<String, String>,
 }
@@ -10,12 +11,13 @@ impl CliTestSetup {
     pub fn new() -> CliTestSetup {
         CliTestSetup {
             program: vec!["sh".to_owned(), "-c".to_owned()],
-            env: HashMap::new(),
+            env: HashMap::<_, _>::new(),
+            cargo_flags: HashSet::<_>::new(),
         }
     }
 
     pub fn run(&self, command: &str) -> Result<CliTestOutput> {
-        const CARGO_RUN_STR: &'static str = "cargo run --";
+        const CARGO_RUN_STR: &'static str = "cargo run";
         let mut cmd = &mut std::process::Command::new(&self.program[0]);
         for arg in self.program.iter().skip(1) {
             cmd = cmd.arg(arg)
@@ -23,7 +25,11 @@ impl CliTestSetup {
         for env in self.env.iter() {
             cmd = cmd.env(env.0, env.1);
         }
-        let output = cmd.arg(format!("{} {}", CARGO_RUN_STR, command)).output()?;
+
+        let cargo_flags = self.cargo_flags.clone();
+        let cargo_flags = cargo_flags.into_iter().collect::<Vec<_>>().join(" ");
+
+        let output = cmd.arg(format!("{} {} -- {}", CARGO_RUN_STR, cargo_flags, command)).output()?;
 
         Ok(CliTestOutput {
             status: output.status,
@@ -48,6 +54,16 @@ impl CliTestSetup {
 
     pub fn with_env(&mut self, name: &str, value: &str) -> &mut Self {
         self.env.insert(name.to_owned(), value.to_owned());
+        self
+    }
+
+    pub fn set_cargo_flags(&mut self, flag: HashSet<String>) -> &mut Self {
+        self.cargo_flags = flag;
+        self
+    }
+
+    pub fn with_cargo_flag(&mut self, flag: &str) -> &mut Self {
+        self.cargo_flags.insert(flag.to_owned());
         self
     }
 }
